d27d69bc8c986ef0bb4fe2494b2fca3ccd7e0e75
yukika <nofate8785@gmail.com>
2021-03-15 21:03:35 +0000

    validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :content, presence: {message: '文字を入力してください'}, length: { maximum: 255, too_long: '最大文字数(255字)を超えています'} 
+  validates :status, presence: {message: '文字を入力してください'}, length: { maximum: 10, too_long: '最大文字数(10字)を超えています'} 
 end
