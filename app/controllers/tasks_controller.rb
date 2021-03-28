318dc13fd0a8336224fbb356f642aee53cb5ff01
yukika <nofate8785@gmail.com>
2021-03-28 07:01:00 +0000

    fix session_new tasks_contoroller tasks_index tasks_form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
