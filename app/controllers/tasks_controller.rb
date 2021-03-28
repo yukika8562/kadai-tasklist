13aabb3f767a42845dec549ae693960e8a2199e2
yukika <nofate8785@gmail.com>
2021-03-28 07:51:22 +0000

    fix tasks_controller tasks_index users_new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,6 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-      @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
